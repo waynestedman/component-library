43bda604075db59cd065e64fcda31309399dc584
Wayne Stedman <waynestedman@users.noreply.github.com>
2023-03-07 17:34:55 -0800

    adj vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ export default defineConfig({
   build: {
     lib: {
       entry: 'src/components/index.ts',
-      formats: ['es'],
+      formats: ['es', 'umd'],
+      name: 'built-case',
     },
     rollupOptions: {
       external: /^lit/,
